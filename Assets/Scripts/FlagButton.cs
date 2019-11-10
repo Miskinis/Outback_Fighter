be15e6c1546905caa7848ad4e69afb6cf04b4f7f
Eimantas Klimavicius <klimavicius.eimantas@gmail.com>
2019-11-10 16:04:44 +0200

    Selection menu fix

--- Assets/Scripts/FlagButton.cs
+++ Assets/Scripts/FlagButton.cs
@@ -1,19 +1,359 @@
+using System;
+using UniRx;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class FlagButton : Button
+[RequireComponent(typeof(RectTransform))]
+public class FlagButton : Selectable, IPointerClickHandler, ISubmitHandler, ICanvasElement
 {
-    protected override void Awake()
+    [NonSerialized] public UnityEvent onClick = new UnityEvent();
+
+    public override void OnSelect(BaseEventData eventData)
     {
-        base.Awake();
-        
-        onClick.AddListener(OnClick);
+        base.OnSelect(eventData);
+        isOn = true;
     }
 
-    private void OnClick()
+    public override void OnDeselect(BaseEventData eventData)
     {
-        var localColors = colors;
-        localColors.disabledColor = Color.white;
-        colors = localColors;
+        base.OnDeselect(eventData);
+        isOn = false;
     }
+
+    protected override void Awake()
+    {
+        this.ObserveEveryValueChanged(x => interactable).Subscribe(value =>
+        {
+            if (value)
+            {
+                m_Group.RegisterFlagButton(this);
+            }
+            else
+            {
+                m_Group.UnregisterFlagButton(this);
+            }
+        });
+    }
+
+    /// <summary>
+        /// Display settings for when a flagButton is activated or deactivated.
+        /// </summary>
+        public enum FlagButtonTransition
+        {
+            /// <summary>
+            /// Show / hide the flagButton instantly
+            /// </summary>
+            None,
+
+            /// <summary>
+            /// Fade the flagButton in / out smoothly.
+            /// </summary>
+            Fade
+        }
+
+        [Serializable]
+        /// <summary>
+        /// UnityEvent callback for when a flagButton is flagButtond.
+        /// </summary>
+        public class FlagButtonEvent : UnityEvent<bool>
+        {}
+
+        /// <summary>
+        /// Transition mode for the flagButton.
+        /// </summary>
+        public FlagButtonTransition flagButtonTransition = FlagButtonTransition.Fade;
+
+        /// <summary>
+        /// Graphic the flagButton should be working with.
+        /// </summary>
+        public Graphic graphic;
+
+        [SerializeField]
+        private FlagButtonGroup m_Group;
+
+        /// <summary>
+        /// Group the flagButton belongs to.
+        /// </summary>
+        public FlagButtonGroup group
+        {
+            get { return m_Group; }
+            set
+            {
+                m_Group = value;
+                SetFlagButtonGroup(m_Group, true);
+                PlayEffect(true);
+            }
+        }
+
+        /// <summary>
+        /// Allow for delegate-based subscriptions for faster events than 'eventReceiver', and allowing for multiple receivers.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// //Attach this script to a FlagButton GameObject. To do this, go to Create>UI>FlagButton.
+        /// //Set your own Text in the Inspector window
+        ///
+        /// using UnityEngine;
+        /// using UnityEngine.UI;
+        ///
+        /// public class Example : MonoBehaviour
+        /// {
+        ///     FlagButton m_FlagButton;
+        ///     public Text m_Text;
+        ///
+        ///     void Start()
+        ///     {
+        ///         //Fetch the FlagButton GameObject
+        ///         m_FlagButton = GetComponent<FlagButton>();
+        ///         //Add listener for when the state of the FlagButton changes, to take action
+        ///         m_FlagButton.onValueChanged.AddListener(delegate {
+        ///                 FlagButtonValueChanged(m_FlagButton);
+        ///             });
+        ///
+        ///         //Initialise the Text to say the first state of the FlagButton
+        ///         m_Text.text = "First Value : " + m_FlagButton.isOn;
+        ///     }
+        ///
+        ///     //Output the new state of the FlagButton into Text
+        ///     void FlagButtonValueChanged(FlagButton change)
+        ///     {
+        ///         m_Text.text =  "New Value : " + m_FlagButton.isOn;
+        ///     }
+        /// }
+        /// </code>
+        /// </example>
+        public FlagButtonEvent onValueChanged = new FlagButtonEvent();
+
+        // Whether the flagButton is on
+        [Tooltip("Is the flagButton currently on or off?")]
+        [SerializeField]
+        private bool m_IsOn;
+
+        protected FlagButton()
+        {}
+
+#if UNITY_EDITOR
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+
+            if (!UnityEditor.PrefabUtility.IsPartOfPrefabAsset(this) && !Application.isPlaying)
+                CanvasUpdateRegistry.RegisterCanvasElementForLayoutRebuild(this);
+        }
+
+#endif // if UNITY_EDITOR
+
+        public virtual void Rebuild(CanvasUpdate executing)
+        {
+#if UNITY_EDITOR
+            if (executing == CanvasUpdate.Prelayout)
+                onValueChanged.Invoke(m_IsOn);
+#endif
+        }
+
+        public virtual void LayoutComplete()
+        {}
+
+        public virtual void GraphicUpdateComplete()
+        {}
+
+        protected override void OnDestroy()
+        {
+            if (m_Group != null)
+                m_Group.EnsureValidState();
+            base.OnDestroy();
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            SetFlagButtonGroup(m_Group, false);
+            PlayEffect(true);
+        }
+
+        protected override void OnDisable()
+        {
+            SetFlagButtonGroup(null, false);
+            base.OnDisable();
+        }
+
+        protected override void OnDidApplyAnimationProperties()
+        {
+            // Check if isOn has been changed by the animation.
+            // Unfortunately there is no way to check if we don�t have a graphic.
+            if (graphic != null)
+            {
+                bool oldValue = !Mathf.Approximately(graphic.canvasRenderer.GetColor().a, 0);
+                if (m_IsOn != oldValue)
+                {
+                    m_IsOn = oldValue;
+                    Set(!oldValue);
+                }
+            }
+
+            base.OnDidApplyAnimationProperties();
+        }
+
+        private void SetFlagButtonGroup(FlagButtonGroup newGroup, bool setMemberValue)
+        {
+            // Sometimes IsActive returns false in OnDisable so don't check for it.
+            // Rather remove the flagButton too often than too little.
+            if (m_Group != null)
+                m_Group.UnregisterFlagButton(this);
+
+            // At runtime the group variable should be set but not when calling this method from OnEnable or OnDisable.
+            // That's why we use the setMemberValue parameter.
+            if (setMemberValue)
+                m_Group = newGroup;
+
+            // Only register to the new group if this FlagButton is active.
+            if (newGroup != null && IsActive())
+                newGroup.RegisterFlagButton(this);
+
+            // If we are in a new group, and this flagButton is on, notify group.
+            // Note: Don't refer to m_Group here as it's not guaranteed to have been set.
+            if (newGroup != null && isOn && IsActive())
+                newGroup.NotifyFlagButtonOn(this);
+        }
+
+        /// <summary>
+        /// Whether the flagButton is currently active.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// /Attach this script to a FlagButton GameObject. To do this, go to Create>UI>FlagButton.
+        /// //Set your own Text in the Inspector window
+        ///
+        /// using UnityEngine;
+        /// using UnityEngine.UI;
+        ///
+        /// public class Example : MonoBehaviour
+        /// {
+        ///     FlagButton m_FlagButton;
+        ///     public Text m_Text;
+        ///
+        ///     void Start()
+        ///     {
+        ///         //Fetch the FlagButton GameObject
+        ///         m_FlagButton = GetComponent<FlagButton>();
+        ///         //Add listener for when the state of the FlagButton changes, and output the state
+        ///         m_FlagButton.onValueChanged.AddListener(delegate {
+        ///                 FlagButtonValueChanged(m_FlagButton);
+        ///             });
+        ///
+        ///         //Initialize the Text to say whether the FlagButton is in a positive or negative state
+        ///         m_Text.text = "FlagButton is : " + m_FlagButton.isOn;
+        ///     }
+        ///
+        ///     //Output the new state of the FlagButton into Text when the user uses the FlagButton
+        ///     void FlagButtonValueChanged(FlagButton change)
+        ///     {
+        ///         m_Text.text =  "FlagButton is : " + m_FlagButton.isOn;
+        ///     }
+        /// }
+        /// </code>
+        /// </example>
+
+        public bool isOn
+        {
+            get { return m_IsOn; }
+
+            set
+            {
+                Set(value);
+            }
+        }
+
+        /// <summary>
+        /// Set isOn without invoking onValueChanged callback.
+        /// </summary>
+        /// <param name="value">New Value for isOn.</param>
+        public void SetIsOnWithoutNotify(bool value)
+        {
+            Set(value, false);
+        }
+
+        void Set(bool value, bool sendCallback = true)
+        {
+            if (m_IsOn == value)
+                return;
+
+            // if we are in a group and set to true, do group logic
+            m_IsOn = value;
+            if (m_Group != null && IsActive())
+            {
+                if (m_IsOn || (!m_Group.AnyFlagButtonsOn() && !m_Group.allowSwitchOff))
+                {
+                    m_IsOn = true;
+                    m_Group.NotifyFlagButtonOn(this, sendCallback);
+                }
+            }
+
+            // Always send event when flagButton is clicked, even if value didn't change
+            // due to already active flagButton in a flagButton group being clicked.
+            // Controls like Dropdown rely on this.
+            // It's up to the user to ignore a selection being set to the same value it already was, if desired.
+            PlayEffect(flagButtonTransition == FlagButtonTransition.None);
+            if (sendCallback)
+            {
+                UISystemProfilerApi.AddMarker("FlagButton.value", this);
+                onValueChanged.Invoke(m_IsOn);
+            }
+        }
+
+        /// <summary>
+        /// Play the appropriate effect.
+        /// </summary>
+        private void PlayEffect(bool instant)
+        {
+            if (graphic == null)
+                return;
+
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+                graphic.canvasRenderer.SetAlpha(m_IsOn ? 1f : 0f);
+            else
+#endif
+            graphic.CrossFadeAlpha(m_IsOn ? 1f : 0f, instant ? 0f : 0.1f, true);
+        }
+
+        /// <summary>
+        /// Assume the correct visual state.
+        /// </summary>
+        protected override void Start()
+        {
+            PlayEffect(true);
+        }
+
+        private void InternalFlagButton()
+        {
+            if (!IsActive() || !IsInteractable())
+                return;
+
+            isOn = !isOn;
+        }
+
+        /// <summary>
+        /// React to clicks.
+        /// </summary>
+        public virtual void OnPointerClick(PointerEventData eventData)
+        {
+            if (eventData.button != PointerEventData.InputButton.Left)
+                return;
+
+            InternalFlagButton();
+        }
+
+        public virtual void OnSubmit(BaseEventData eventData)
+        {
+            InternalFlagButton();
+            
+            var localColors = colors;
+            localColors.disabledColor = Color.white;
+            colors = localColors;
+        
+            onClick.Invoke();
+        }
 }
---
+++ Assets/Scripts/FlagButtonGroup.cs
@@ -0,0 +1,183 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent]
+public class FlagButtonGroup : UIBehaviour
+{
+    [SerializeField] private bool m_AllowSwitchOff = false;
+
+    /// <summary>
+    /// Is it allowed that no flagButton is switched on?
+    /// </summary>
+    /// <remarks>
+    /// If this setting is enabled, pressing the flagButton that is currently switched on will switch it off, so that no flagButton is switched on. If this setting is disabled, pressing the flagButton that is currently switched on will not change its state.
+    /// Note that even if allowSwitchOff is false, the FlagButton Group will not enforce its constraint right away if no flagButtons in the group are switched on when the scene is loaded or when the group is instantiated. It will only prevent the user from switching a flagButton off.
+    /// </remarks>
+    public bool allowSwitchOff
+    {
+        get { return m_AllowSwitchOff; }
+        set { m_AllowSwitchOff = value; }
+    }
+
+    private List<FlagButton> m_FlagButtons = new List<FlagButton>();
+
+    public void UpdateSelectablesNavigation()
+    {
+        for (var i = 0; i < m_FlagButtons.Count; i++)
+        {
+            var flagButton = m_FlagButtons[i];
+            var navigation = new Navigation {mode = Navigation.Mode.Explicit};
+
+            if(flagButton.interactable == false) continue;
+            
+            if (i == 0)
+            {
+                navigation.selectOnRight = m_FlagButtons[m_FlagButtons.Count - 1];
+            }
+            else
+            {
+                navigation.selectOnRight = m_FlagButtons[i - 1];
+            }
+
+            if (i + 1 >= m_FlagButtons.Count)
+            {
+                navigation.selectOnLeft = m_FlagButtons[0];
+            }
+            else if(i + 1 < m_FlagButtons.Count)
+            {
+                navigation.selectOnLeft = m_FlagButtons[i + 1];
+            }
+            flagButton.navigation = navigation;
+        }
+    }
+
+    protected FlagButtonGroup()
+    {
+    }
+
+    /// <summary>
+    /// Because all the FlagButtons have registered themselves in the OnEnabled, Start should check to
+    /// make sure at least one FlagButton is active in groups that do not AllowSwitchOff
+    /// </summary>
+    protected override void Start()
+    {
+        EnsureValidState();
+        base.Start();
+    }
+
+    private void ValidateFlagButtonIsInGroup(FlagButton flagButton)
+    {
+        if (flagButton == null || !m_FlagButtons.Contains(flagButton))
+            throw new ArgumentException(string.Format("FlagButton {0} is not part of FlagButtonGroup {1}", new object[] {flagButton, this}));
+    }
+
+    /// <summary>
+    /// Notify the group that the given flagButton is enabled.
+    /// </summary>
+    /// <param name="flagButton">The flagButton that got triggered on.</param>
+    /// <param name="sendCallback">If other flagButtons should send onValueChanged.</param>
+    public void NotifyFlagButtonOn(FlagButton flagButton, bool sendCallback = true)
+    {
+        ValidateFlagButtonIsInGroup(flagButton);
+        // disable all flagButtons in the group
+        for (var i = 0; i < m_FlagButtons.Count; i++)
+        {
+            if (m_FlagButtons[i] == flagButton)
+                continue;
+
+            if (sendCallback)
+                m_FlagButtons[i].isOn = false;
+            else
+                m_FlagButtons[i].SetIsOnWithoutNotify(false);
+        }
+    }
+
+    /// <summary>
+    /// Unregister a flagButton from the group.
+    /// </summary>
+    /// <param name="flagButton">The flagButton to remove.</param>
+    public void UnregisterFlagButton(FlagButton flagButton)
+    {
+        if (m_FlagButtons.Contains(flagButton))
+        {
+            m_FlagButtons.Remove(flagButton);
+            UpdateSelectablesNavigation();
+        }
+    }
+
+    /// <summary>
+    /// Register a flagButton with the flagButton group so it is watched for changes and notified if another flagButton in the group changes.
+    /// </summary>
+    /// <param name="flagButton">The flagButton to register with the group.</param>
+    public void RegisterFlagButton(FlagButton flagButton)
+    {
+        if (!m_FlagButtons.Contains(flagButton))
+        {
+            m_FlagButtons.Add(flagButton);
+            UpdateSelectablesNavigation();
+        }
+    }
+
+    /// <summary>
+    /// Ensure that the flagButton group still has a valid state. This is only relevant when a FlagButtonGroup is Started
+    /// or a FlagButton has been deleted from the group.
+    /// </summary>
+    public void EnsureValidState()
+    {
+        if (!allowSwitchOff && !AnyFlagButtonsOn() && m_FlagButtons.Count != 0)
+        {
+            m_FlagButtons[0].isOn = true;
+            NotifyFlagButtonOn(m_FlagButtons[0]);
+        }
+    }
+
+    /// <summary>
+    /// Are any of the flagButtons on?
+    /// </summary>
+    /// <returns>Are and of the flagButtons on?</returns>
+    public bool AnyFlagButtonsOn()
+    {
+        return m_FlagButtons.Find(x => x.isOn) != null;
+    }
+
+    /// <summary>
+    /// Returns the flagButtons in this group that are active.
+    /// </summary>
+    /// <returns>The active flagButtons in the group.</returns>
+    /// <remarks>
+    /// FlagButtons belonging to this group but are not active either because their GameObject is inactive or because the FlagButton component is disabled, are not returned as part of the list.
+    /// </remarks>
+    public IEnumerable<FlagButton> ActiveFlagButtons()
+    {
+        return m_FlagButtons.Where(x => x.isOn);
+    }
+
+    /// <summary>
+    /// Switch all flagButtons off.
+    /// </summary>
+    /// <remarks>
+    /// This method can be used to switch all flagButtons off, regardless of whether the allowSwitchOff property is enabled or not.
+    /// </remarks>
+    public void SetAllFlagButtonsOff(bool sendCallback = true)
+    {
+        bool oldAllowSwitchOff = m_AllowSwitchOff;
+        m_AllowSwitchOff = true;
+
+        if (sendCallback)
+        {
+            for (var i = 0; i < m_FlagButtons.Count; i++)
+                m_FlagButtons[i].isOn = false;
+        }
+        else
+        {
+            for (var i = 0; i < m_FlagButtons.Count; i++)
+                m_FlagButtons[i].SetIsOnWithoutNotify(false);
+        }
+
+        m_AllowSwitchOff = oldAllowSwitchOff;
+    }
+}
