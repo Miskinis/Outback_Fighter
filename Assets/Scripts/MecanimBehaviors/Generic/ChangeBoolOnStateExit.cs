80d1716b4de7fe2075af8fc9ec4007bda4b3e329
Eimantas <klimavicius.eimantas@gmail.com>
2019-11-21 18:36:05 +0200

    Game restart fix

---
+++ Assets/Scripts/MecanimBehaviors/Generic/ChangeBoolOnStateExit.cs
@@ -0,0 +1,24 @@
+﻿using Plugins.SerializableDictionary;
+using UnityEngine;
+
+namespace MecanimBehaviors.Generic
+{
+    public class ChangeBoolOnStateExit : StateMachineBehaviour
+    {
+        [Tooltip("Bool parameter names and values to change")]
+        public StringBoolDictionary parameters;
+
+        private IntBoolDictionary _parameters;
+
+        private void Awake()
+        {
+            _parameters = new IntBoolDictionary();
+            foreach (var parameter in parameters) _parameters.Add(Animator.StringToHash(parameter.Key), parameter.Value);
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            foreach (var parameter in _parameters) animator.SetBool(parameter.Key, parameter.Value);
+        }
+    }
+}
